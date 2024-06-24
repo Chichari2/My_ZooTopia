ccee2d26593372ed8717c682bbdebc34b9c3019d
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-06-24 11:50:42 +0200

    add except

--- my_zootopia.py
+++ my_zootopia.py
@@ -1,17 +1,16 @@
 import requests
-import certifi
 
 def main():
-  animal_name = input("Enter animal name: ")
+  animal_name = input("Enter a name of an animal: ")
   response = requests.get(
-
-    f"https://api.api-ninjas.com/v1/animals?name={animal_name}&X-Api-Key=9UaBWKTevqExmKLM6ZOCJQ==Ut1IWMTAbSsgpk9U",
-    verify=certifi.where(),
-    timeout=10
+  f"https://api.api-ninjas.com/v1/animals?name={animal_name}&X-Api-Key=9UaBWKTevqExmKLM6ZOCJQ==Ut1IWMTAbSsgpk9U"
   )
   response.raise_for_status()
   data = response.json()
-  print(data)
+  if data == []:
+    print(f"<h2>The animal {animal_name} doesn't exist.</h2>")
+  else:
+    print(data)
 
 
 if __name__ == "__main__":
